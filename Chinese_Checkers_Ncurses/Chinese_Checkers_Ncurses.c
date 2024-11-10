6a5adc5fc02596d1e7876b85d2c62148c2f7a38e
Ashraf Allie <ashraf.allie01@gmail.com>
2024-11-10 11:35:51 +0200

    Update Chinese_Checkers_Ncurses.c

--- Chinese_Checkers_Ncurses/Chinese_Checkers_Ncurses.c
+++ Chinese_Checkers_Ncurses/Chinese_Checkers_Ncurses.c
@@ -7,23 +7,13 @@
           bead use an adjacent bead to jump over the intended bead to be
           eliminated. A bead may only jump horizontally or vertically over 1 or
           more beads provided that there is an empty space in front of the bead
-          that is intended to be eliminated
-
-
-
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+          that is intended to be eliminated.
+          
+          
+          This program is free software: you can redistribute it and/or modify
+          it. This program is distributed in the hope that it will be useful,
+          but WITHOUT ANY WARRANTY; without even the implied warranty of
+          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 */
 
 //----------------
